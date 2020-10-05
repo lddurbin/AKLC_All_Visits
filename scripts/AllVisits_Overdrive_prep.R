e055db820429279feafaa391ab14acb2f3551287
lddurbin <l.d.durbin@gmail.com>
2020-10-06 11:32:03 +1300

    Exclude AdHoc Analysis from repository, and fixed bug in Overdrive script.

--- scripts/AllVisits_Overdrive_prep.R
+++ scripts/AllVisits_Overdrive_prep.R
@@ -1,7 +1,9 @@
 # Load and clean the Overdrive data
 Overdrive <- excel_sheets(AllVisits.files.local["Overdrive"]) %>%
   set_names() %>%
-  discard(. %in% c("Source", "Since inception", "2015", "2016", "2017")) %>% 
+  as_tibble() %>% 
+  filter(!value %in% c("Source", "Since inception", "2015", "2016", "2017")) %>% 
+  pull() %>% 
   map(read_excel, path = AllVisits.files.local["Overdrive"]) %>% 
   bind_rows() %>% 
   clean_names() %>% 
