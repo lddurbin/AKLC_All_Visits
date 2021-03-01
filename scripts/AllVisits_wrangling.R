cfb7a318b72865627f69921f838688146cf4f99e
lddurbin <l.d.durbin@gmail.com>
2021-03-01 14:41:27 +1300

    Bind all data frames together, and prep for output.

--- scripts/AllVisits_wrangling.R
+++ scripts/AllVisits_wrangling.R
@@ -21,30 +21,44 @@ source("scripts/AllVisits_copy_files.R")
 # *****************************************************************************
 # Load and prep data ---- 
 
+# Record and Archives (enquiries and website sessions)
 source("scripts/AllVisits_AandR_prep.R")
 
+# Library app
 source("scripts/AllVisits_Boopsie_prep.R")
 
+# Outreach participation (community libraries, mobile and access, research and heritage)
 source("scripts/AllVisits_DX_prep.R")
 
+# Research and Heritage Facebook
 source("scripts/AllVisits_HeritageSocial_prep.R")
 
+# Website sessions for Kura, Manuscripts Online, and Heritage Images
 source("scripts/AllVisits_Kura_ManuscriptsOnline_HeritageImages_prep.R")
 
+# Answered calls on Library Connect
 source("scripts/AllVisits_LibraryConnect_prep.R")
 
+# Daily visits to the Overdrive website
 source("scripts/AllVisits_Overdrive_prep.R")
 
+# Regional social media engagement, views, listens
 source("scripts/AllVisits_RegionalSocial_prep.R")
 
+# Subscription databases sessions
 source("scripts/AllVisits_SubscriptionDatabases_prep.R")
-
-#assign labels to metrics
-#source("scripts/definitions.R")
 
 # *****************************************************************************
 
-bind_rows(
+
+# *****************************************************************************
+# Create and save output file ---- 
+
+categories <- read_excel("data/AllVisits_data_sources.xlsx", sheet="Categories", col_types = "text") %>% 
+  clean_names()
+
+# Create one data frame for everything, assign IDs to each service, join with categories, add date, replace NAs with 0, filter out current month
+AllVisits <- bind_rows(
   AandR_enquiries,
   AandR_sessions,
   Boopsie,
@@ -64,59 +78,20 @@ bind_rows(
   RegionalYouTube,
   research_outreach,
   Subscriptions,
-  .id = "id") %>% View()
-
-
-# *****************************************************************************
-# Finishing touches and export ---- 
-
-# Merge all data frames together, add various date fields, filter to relevant FYs, insert rows for missing data
-AllVisits <- rbind(Kura, HeritageImages, ManuscriptsOnline, Boopsie2019, Boopsie2020, RegionalSocial, Overdrive, HeritageSocial, Subscriptions, DX, AandR_enquiries, AandR_sessions, LibraryConnect) %>% 
-  mutate(Date = as.Date(paste(Month, "01", Year, sep="/"), format="%b/%d/%Y")) %>% 
-  complete(Date, nesting(Metric_type, Metric_source, Metric_group), fill = list(Metric = 0)) %>% 
-  mutate(Month = case_when(is.na(Month) ~ format(Date, "%b"), !is.na(Month) ~ Month),
-         Year = case_when(is.na(Year) ~ format(Date, "%Y"), !is.na(Year) ~ Year),
-         FY = as.character(get_fy(Date)),
-         Month_num = match(Month, month.abb),
-         Month_number = case_when(Month_num > 6 ~ (Month_num)-6, Month_num < 7 ~ (Month_num)+6)
-         ) %>% 
-  select(-Month_num) %>% 
-  filter(Date != as.Date(format(Sys.Date(), paste("%Y-%m", "01", sep="-"))) & FY %in% c("2019", "2020", "2021"))
-
-#Calculate the change (# and %) compared to the same point the previous FY
-AllVisits <- arrange(AllVisits, Metric_source, Month_number, FY) %>%
-  group_by(Metric_source, Month) %>%
-  dplyr::mutate(Change_num = Metric - lag(Metric, default = first(Metric)),
-                Change_perc = Change_num/lag(Metric, default = first(Metric))) %>% 
-  ungroup()
-
-#Calculate the change (# and %) compared to the previous month
-AllVisits <- arrange(AllVisits, Metric_source, FY, Month_number) %>%
-  group_by(Metric_source) %>%
-  dplyr::mutate(Change_month_num = Metric - lag(Metric, default = first(Metric)),
-                Change_month_perc = Change_month_num/lag(Metric, default = first(Metric))) %>% 
-  ungroup()
-
-#Calculate the FYTD
-AllVisits <- arrange(AllVisits, Metric_source, FY, Month_number) %>%
-  group_by(Metric_source, FY) %>%
-  dplyr::mutate(FYTD = cumsum(Metric)) %>% 
-  ungroup()
-
-#Calculate the change (# and %) compared to the previous FYTD
-AllVisits <- arrange(AllVisits, Metric_source, Month_number, FY) %>%
-  group_by(Metric_source, Month) %>%
-  dplyr::mutate(Change_FYTD_num = FYTD - lag(FYTD, default = first(FYTD)),
-                Change_FYTD_perc = Change_FYTD_num/lag(FYTD, default = first(FYTD))) %>% 
-  ungroup()
+  .id = "id") %>% 
+  left_join(categories, by = "id") %>% 
+  mutate(date = as.Date(paste(Month, "01", Year, sep="/"), format="%b/%d/%Y")) %>% 
+  select(-Month, -Year, metric = Metric) %>% 
+  complete(fill = list(Metric = 0)) %>% 
+  filter(date != as.Date(format(Sys.Date(), paste("%Y-%m", "01", sep="-"))))
 
 # Change infinite values to zero
 AllVisits[AllVisits == Inf] <- 0
 
 #Archive the old data output and export the new data frame to a .csv file
 file.rename("data/processed/AllVisits.csv", paste("data/processed/archived/AllVisits_", as.Date(file.info("data/processed/AllVisits.csv")$ctime), ".csv", sep=""))
+
 AllVisits %>%
-  select(Data_source, Metric_source, Month, Year, Metric,	Metric_type,	Metric_group,	Month_number,	FY,	Date,	Change_num,	Change_perc, Change_month_num, Change_month_perc, FYTD, Change_FYTD_num, Change_FYTD_perc) %>%
   write.csv("data/processed/AllVisits.csv", na="")
 
 # *****************************************************************************
